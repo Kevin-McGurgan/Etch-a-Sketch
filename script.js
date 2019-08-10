375d0515ed94728580ee9ddd4e4610fcaea953a5
Kevin McGurgan <kevin.mcgurgan@gmail.com>
2019-08-10 14:59:48 -0400

    add event listeners to create hover state and a reset button

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const container = document.querySelector("#container");
 
 createGrid(16);
 
+const cellHover = document.querySelectorAll(".gridCell");
+
+const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", () => {
+    reset();
+  });
+
+for (var i = 0; i < cellHover.length; i++) {
+  cellHover[i].addEventListener("mouseover", function() {
+    this.classList.remove("gridCell");
+    this.classList.add("gridCellHover");
+  });
+}
+
+function reset() {
+  for (var i = 0; i < cellHover.length; i++) {
+      cellHover[i].classList.remove("gridCellHover");
+      cellHover[i].classList.add("gridCell");
+  }
+}
+
 function createGrid(num) {
   for (i=1; i <= num; i++) {
     const row = document.createElement("div");
